40821e9d5dbbd5c9a6304936a89eae5e426b9858
kiforks <ks.kifor@gmail.com>
2020-07-07 12:21:44 +0300

    Add preloader

--- src/js/scripts/star-control.js
+++ src/js/scripts/star-control.js
@@ -1,22 +1,32 @@
 function sectionsToggle(selector, button, parent) {
   let toggleButton = document.querySelectorAll('.' + button);
+  // let star = document.querySelectorAll('.specification__star');
 
   toggleButton.forEach(button => {
     button.onclick = function(event) {
       let parentSelector = event.target.closest('.' + parent);
       let classList = parentSelector.classList;
       let activeSelector = selector + '--active';
-      let star = parentSelector.querySelectorAll('.specification__star');
-      let starBox = parentSelector.querySelector('.sections__star-box');
-      let starActive = 'sections__star-box--active';
-      let mainStarActive = 'specification__star--active';
-
-      // console.log(star);
-      // console.log(parentSelector.classList.contains('123'))
+      // let star = parentSelector.querySelector('.specification__star');
+      // let starClone = parentSelector.querySelector('.sections__star-box');
 
       classList.contains(activeSelector) ? classList.remove(activeSelector) : classList.add(activeSelector);
+
+      // star.onclick = function (event) {
+      //   if(star.classList.contains('specification__star--active')) {
+      //     starClone.classList.add('sections__star-box--active');
+      //   } else {
+      //     starClone.classList.remove('sections__star-box--active');
+      //   }
+      // }
     };
   });
+
+  // star.forEach(star => {
+  //   star.onclick = function (event) {
+  //
+  //   }
+  // })
 }
 
 sectionsToggle('sections__item', 'sections__picture', 'sections__item');
