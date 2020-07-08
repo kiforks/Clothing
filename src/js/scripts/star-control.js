019690871a36783c4c460cf3601fd4d2840becc0
kiforks <ks.kifor@gmail.com>
2020-07-08 11:25:20 +0300

    Done

--- src/js/scripts/star-control.js
+++ src/js/scripts/star-control.js
@@ -1,6 +1,7 @@
 function sectionsToggle(button, selector, parent) {
   let toggleButton = document.querySelectorAll('.' + button);
   let modalWindow = document.querySelector('.specification');
+  let sectionsContainer = document.querySelector('.sections__container');
 
   toggleButton.forEach(button => {
     button.onclick = event => {
@@ -8,9 +9,6 @@ function sectionsToggle(button, selector, parent) {
       let toggleSelector = parentSelector.querySelector('.' + selector);
       let toggleClass = toggleSelector.classList;
       let activeSelector = selector + '--active';
-      let star = parentSelector.querySelector('.specification__star');
-      let cloneStar = parentSelector.querySelector('.sections__star-box');
-
 
       if(button.classList.contains('sections__picture')) {
         toggleSelector.appendChild(modalWindow);
@@ -18,20 +16,54 @@ function sectionsToggle(button, selector, parent) {
         toggleSelector.removeChild(modalWindow);
       }
 
-      toggleClass.contains(activeSelector) ? toggleClass.remove(activeSelector) : toggleClass.add(activeSelector);
+      modalWindow.scrollIntoView();
 
-      if(star.classList.contains('specification__star--active')) {
-        cloneStar.classList.add('sections__star-box--active')
+      if(toggleClass.contains(activeSelector)) {
+        toggleClass.remove(activeSelector);
+        parentSelector.removeAttribute('style');
       } else {
-        cloneStar.classList.remove('sections__star-box--active')
+        toggleClass.add(activeSelector);
+        parentSelector.style = 'z-index: 10000';
+      }
+
+      // toggleClass.contains(activeSelector) ? toggleClass.remove(activeSelector) : toggleClass.add(activeSelector);
+
+      let parentStar = parentSelector.querySelector('.sections__star-box');
+      let star = document.querySelector('.specification__star');
+
+      if(event.target.classList.contains('specification__button')) {
+        return;
+      }
+
+      if(parentStar.classList.contains('sections__star-box--active')) {
+        star.classList.add('specification__star--active');
+      } else {
+        star.classList.remove('specification__star--active');
       }
     }
   })
+}
+
+function favoriteControl() {
+  let star = document.querySelector('.specification__star');
+
+  star && star.addEventListener('click', () => {
+    let parent = star.parentElement.parentElement.parentElement;
+    let cloneStar = parent.querySelector('.sections__star-box');
+
+    if(!star.classList.contains('specification__star--active')) {
+      star.classList.add('specification__star--active');
+      cloneStar.classList.add('sections__star-box--active');
+    } else {
+      star.classList.remove('specification__star--active');
+      cloneStar.classList.remove('sections__star-box--active');
+    }
+  });
 }
 
 sectionsToggle('sections__picture', 'sections__wrapper',
   'sections__item');
 sectionsToggle('specification__button', 'sections__wrapper',
   'sections__item');
-sectionsToggle('specification__star', 'specification__star',
-  'sections__item');
+
+favoriteControl();
